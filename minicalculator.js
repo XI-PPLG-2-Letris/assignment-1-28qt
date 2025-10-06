0da84f42790169e6a595202c0ae5b8895a6403fe
28qt <renoyaniririn@gmail.com>
2025-10-06 09:35:29 +0700

    Add files via upload
    Nama:Reno Dwi Putra
    Kelas:11 pplg 2

---
+++ minicalculator.js
@@ -0,0 +1,32 @@
+a = 10;
+b = 20;
+
+const penjumlahan = () => {
+  console.log(`penjumlahan ${a} + ${b} adalah ${a + b}`);
+};
+
+penjumlahan();
+const pengurangan = () => {
+  console.log(`penjumlahan ${a} - ${b} adalah ${a - b}`);
+};
+
+pengurangan();
+const perkalian = () => {
+  console.log(`penjumlahan ${a} * ${b} adalah ${a * b}`);
+};
+
+perkalian();
+const pangkat = () => {
+  console.log(`penjumlahan ${a} ** ${b} adalah ${a ** b}`);
+};
+
+pangkat();
+const modulus = () => {
+  console.log(`penjumlahan ${a} % ${b} adalah ${a % b}`);
+};
+
+pangkat();
+const pembagian = () => {
+  console.log(`penjumlahan ${a} / ${b} adalah ${a / b}`);
+};
+pembagian();
